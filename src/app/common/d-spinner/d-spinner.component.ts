5b7e5d2aab2fe4eb7f6f78be5dc364f077678a5f
Michał Dopieralski <dopieralskimichal@gmail.com>
2017-02-15 10:25:42 +0100

    feat(common/d-spinner): simple loader

---
+++ src/app/common/d-spinner/d-spinner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+require('./d-spinner.less');
+
+@Component({
+    selector: 'd-spinner',
+    template: '<div class="d-spinner"></div>'
+})
+export class dSpinnerComponent {
+    name = 'd-spinner';
+}
