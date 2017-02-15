6e0ea8ec7b6744de5c81d18cf1766599186716e2
Michał Dopieralski <dopieralskimichal@gmail.com>
2017-02-15 12:17:51 +0100

    ci(travis): running tests on firefox

--- karma.config.js
+++ karma.config.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: ['Chrome'],
+        browsers: ['Firefox'],
         singleRun: true,
         concurrency: Infinity
     });
